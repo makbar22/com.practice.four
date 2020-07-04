4c46358b666e31d6dc46647cd65b08bb701c6866
Murtaza Akbar <makbar22>
2020-07-03 22:28:19 -0400

    practice four

---
+++ src/main/java/com/practice/four/com/practice/four/App.java
@@ -0,0 +1,13 @@
+package com.practice.four.com.practice.four;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/practice/four/com/practice/four/AppTest.java
@@ -0,0 +1,20 @@
+package com.practice.four.com.practice.four;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
